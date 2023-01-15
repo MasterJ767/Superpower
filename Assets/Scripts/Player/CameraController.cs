e4e00ff39fcde8733801596d761a2ff20cecaa22
MasterJ767 <jon.arundel@hotmail.com>
2023-01-15 20:46:14 +0000

    Add Camera zoom control

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,15 +7,18 @@ public class CameraController : MonoBehaviour {
     [Header("Settings")]
     public Transform followTarget;
     public float rotationSpeed = 2.0f;
-    public float followDistance;
+    public float minfollowDistance = 2.0f;
+    public float maxfollowDistance = 5.0f;
     public Vector2 framingOffset;
     public float minVerticalAngle = -45.0f;
     public float maxVerticalAngle = 45.0f;
     public bool invertX;
     public bool invertY;
+    public bool invertZoom;
 
     private float rotationX;
     private float rotationY;
+    private float zoom = 0f;
 
     public Quaternion PlanarRotation => Quaternion.Euler(0, rotationY, 0);
 
@@ -27,16 +30,19 @@ public class CameraController : MonoBehaviour {
     private void Update() {
         float invertXVal = invertX ? -1 : 1; 
         float invertYVal = invertY ? -1 : 1; 
-
         rotationX += Input.GetAxis("Mouse Y") * rotationSpeed * invertXVal;
         rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
         rotationY += Input.GetAxis("Mouse X") * rotationSpeed * invertYVal;
 
-        Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        float invertZoomVal = invertZoom ? -1 : 1;
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel") * invertZoomVal;
+        zoom = Mathf.Clamp01(zoom + zoomDelta);
+        float followDistance = Mathf.Lerp(maxfollowDistance, minfollowDistance, zoom); 
 
+        Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
         Vector3 focusPosition = followTarget.position + new Vector3(framingOffset.x, framingOffset.y, 0);
         transform.position = focusPosition - (targetRotation * new Vector3(0, 0, followDistance));
-        transform.rotation = targetRotation;
+        transform.rotation = targetRotation;   
     }
 }
 }
